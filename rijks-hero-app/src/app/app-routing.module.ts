f680d87681a2b03347230cebc54a3c8d6365a403
JorisPaarde <jorispaarde@gmail.com>
2022-01-28 10:51:20 +0100

    added navigation

--- rijks-hero-app/src/app/app-routing.module.ts
+++ rijks-hero-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtObjectsComponent } from './art-objects/art-objects.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'art-objects', component: ArtObjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
